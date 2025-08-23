fa7f39461297b56b0dd4b106e83777222c3a397b
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2025-08-23 16:45:32 +0200

    added uvucorn dunder main

--- fantasy-sci-fi-ai-assistant/main.py
+++ fantasy-sci-fi-ai-assistant/main.py
@@ -26,3 +26,7 @@ app.include_router(documents.router)
 @app.get("/")
 async def root():
     return {"message" : "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
